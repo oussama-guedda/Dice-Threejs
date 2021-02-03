ab3ba08a91c3994e8ab674ef87765c304222fd0c
Nicolas <nicolas.trochut@orange.fr>
2021-02-03 18:08:39 +0100

    Ajout d'un dé 3D tout fait

--- dice.js
+++ dice.js
@@ -8,34 +8,59 @@ scene.background = new THREE.Color(0, 0, 0);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
-/*const loader = new THREE.CubeTextureLoader();
-loader.setPath( 'textures/' );
-loader.load([
-    '1.png', '2.png',
-    '3.png', '4.png',
-    '5.png', '6.png'
-], function(textures) {
-    const material = new THREE.MeshBasicMaterial( { envMap: textures } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+/*const loadManager = new THREE.LoadingManager();
+const loader = new THREE.TextureLoader(loadManager);
 
-    renderer.render(scene, camera);
-});*/
+const materials = [
+    new THREE.MeshBasicMaterial({map: loader.load('textures/1.png')}),
+    new THREE.MeshBasicMaterial({map: loader.load('textures/2.png')}),
+    new THREE.MeshBasicMaterial({map: loader.load('textures/3.png')}),
+    new THREE.MeshBasicMaterial({map: loader.load('textures/4.png')}),
+    new THREE.MeshBasicMaterial({map: loader.load('textures/5.png')}),
+    new THREE.MeshBasicMaterial({map: loader.load('textures/6.png')}),
+];
 
-const loader = new THREE.TextureLoader();
-loader.load('textures/1.png', function(texture) {
-    const material = new THREE.MeshBasicMaterial({
-        map: texture,
-    });
-    const cube = new THREE.Mesh(geometry, material);
+loadManager.onLoad = () => {
+    const cube = new THREE.Mesh(geometry, materials);
     scene.add(cube);
-});
+
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.autoRotate = true;
+    controls.minDistance = 2;
+    controls.maxDistance = 30;
+
+    function animate() {
+        controls.update();
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+};*/
+
+var loader = new THREE.GLTFLoader();
+
+loader.load('model/dice.glb', function (gltf) {
+        scene.add(gltf.scene);
+
+        var controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.enablePan = false;
+        controls.update();
+        controls.minDistance = 2;
+        controls.maxDistance = 30;
+
+        function animate() {
+            controls.update();
+
+            gltf.scene.rotateY(.05);
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    },
+    undefined,
+    function (error) {
+        console.error(error);
+    });
 
 camera.position.z = 10;
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-animate();
